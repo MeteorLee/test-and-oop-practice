bd83401fab048aed5bc9561ed2c033c966d642bb
MeteorLee <meteorproject529@gmail.com>
2023-05-24 01:06:53 +0900

    refactor: 길이 부분 중복 공통화 후 테스트 통과

--- src/main/java/org/example/PasswordValidator.java
+++ src/main/java/org/example/PasswordValidator.java
@@ -3,7 +3,9 @@ package org.example;
 public class PasswordValidator {
 
     public static void validate(String password) {
-         if (password.length() < 8 || password.length() > 12) {
+        int length = password.length();
+
+        if (length < 8 || length > 12) {
              throw new IllegalArgumentException("비밀번호는 최소 8자 이상 최대 12자 이하여야 한다.");
          }
     }
