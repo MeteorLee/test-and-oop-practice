3d7b9aed1ace6244d61da2d99426c434374a7b95
MeteorLee <meteorproject529@gmail.com>
2023-05-24 01:04:55 +0900

    feat: 프러덕션 코드 생성 후 테스트 통과

--- src/main/java/org/example/PasswordValidator.java
+++ src/main/java/org/example/PasswordValidator.java
@@ -3,6 +3,8 @@ package org.example;
 public class PasswordValidator {
 
     public static void validate(String password) {
-         
+         if (password.length() < 8 || password.length() > 12) {
+             throw new IllegalArgumentException("비밀번호는 최소 8자 이상 최대 12자 이하여야 한다.");
+         }
     }
 }
