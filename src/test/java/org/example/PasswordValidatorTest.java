2e59762a91e46efca1097870def4f3a2f30479ff
MeteorLee <meteorproject529@gmail.com>
2023-05-24 01:20:32 +0900

    test: 비밀 번호 자릿수가 맞지 않는 경우

--- src/test/java/org/example/PasswordValidatorTest.java
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -18,4 +18,14 @@ public class PasswordValidatorTest {
         assertThatCode(() -> PasswordValidator.validate("meteorLee"))
                 .doesNotThrowAnyException();
     }
+
+    @DisplayName("비밀번호가 8자 미만 또는 12자 초과하는 경우 IllegalArgumentException 예외가 발생한다.")
+    @Test
+    void validatePasswordTest2() {
+
+        assertThatCode(() -> PasswordValidator.validate("aabb"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("비밀번호는 최소 8자 이상 최대 12자 이하여야 한다.");
+    }
+
 }
