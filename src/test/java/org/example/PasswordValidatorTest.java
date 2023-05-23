5c025df722158db7bdc8bae80831ad01fd9963c7
MeteorLee <meteorproject529@gmail.com>
2023-05-24 00:57:50 +0900

    feat: tdd 연습 클래스 생성 + 조건 주석 처리

---
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -0,0 +1,9 @@
+package org.example;
+
+/**
+ * 비밀번호는 최소 8자 이상 12자 이하여야 한다.
+ * 비밀번호가 8자 미만 또는 12자 초과인 경우 IllegalArgumentException 예외를 발생시킨다.
+ * 경계조건에 대해 테스트 코드를 작성해야 한다.
+ */
+public class PasswordValidatorTest {
+}
