748e43ecb393d8a697a851cb8b5976cf5bf24264
MeteorLee <meteorproject529@gmail.com>
2023-05-27 01:27:30 +0900

    refactor: 학점 계산기 클래스 생성, 테스트를 통과할 수 있도록 학점 계산 메서드 구현
    일단 테스트를 통과할 수 있도록 return 4.5; 로 구현

---
+++ src/main/java/org/example/oop_practice/gradecalculator/GradeCalculator.java
@@ -0,0 +1,16 @@
+package org.example.oop_practice.gradecalculator;
+
+import java.util.List;
+
+public class GradeCalculator {
+
+    private final List<Course> courses;
+
+    public GradeCalculator(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public double calculateGrade() {
+        return 4.5;
+    }
+}
